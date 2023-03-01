76a3e0365bc99dba3745643b3841e4ca99748c4a
ansal.ibrahim <32477006+ansal-exp@users.noreply.github.com>
2023-03-01 13:17:01 +0530

    update lib

---
+++ dist/entry.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "DatePicker", {
+  enumerable: true,
+  get: function get() {
+    return _DatePicker["default"];
+  }
+});
+exports["default"] = void 0;
+require("react-calendar/dist/Calendar.css");
+var _DatePicker = _interopRequireDefault(require("./DatePicker"));
+require("./DatePicker.css");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+// File is created during build phase and placed in dist directory
+// eslint-disable-next-line import/no-unresolved
+var _default = _DatePicker["default"];
+exports["default"] = _default;
---
+++ dist/entry.nostyle.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "DatePicker", {
+  enumerable: true,
+  get: function get() {
+    return _DatePicker["default"];
+  }
+});
+exports["default"] = void 0;
+var _DatePicker = _interopRequireDefault(require("./DatePicker"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var _default = _DatePicker["default"];
+exports["default"] = _default;
